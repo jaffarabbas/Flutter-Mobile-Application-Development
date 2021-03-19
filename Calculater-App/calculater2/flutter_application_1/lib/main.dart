e8966ab6ef509fe5ee258548c11a0817674532bd
jaffarabbas <gamaportal8@gmail.com>
2021-03-19 16:54:13 +0500

    3-19-2021  Final Commit

--- Calculater-App/calculater2/flutter_application_1/lib/main.dart
+++ Calculater-App/calculater2/flutter_application_1/lib/main.dart
@@ -1,2 +1,316 @@
+import 'package:flutter/material.dart';
 
+void main() => runApp(MyApp());
 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Calculater(),
+    );
+  }
+}
+
+class Calculater extends StatefulWidget {
+  @override
+  _CalculaterState createState() => _CalculaterState();
+}
+
+class _CalculaterState extends State<Calculater> {
+  //Calculator logic
+  dynamic text = '';
+  dynamic runtime = '';
+  double numOne = 0;
+  double numTwo = 0;
+
+  dynamic result = '';
+  dynamic finalResult = '';
+  dynamic opr = '';
+  dynamic preOpr = '';
+  dynamic displaytxt = 20;
+
+  void calculation(btnText) {
+    if (btnText == 'AC') {
+      text = '0';
+      numOne = 0;
+      numTwo = 0;
+      result = '';
+      finalResult = '0';
+      opr = '';
+      preOpr = '';
+    } else if (opr == '=' && btnText == '=') {
+      if (preOpr == '+') {
+        finalResult = add();
+      } else if (preOpr == '-') {
+        finalResult = sub();
+      } else if (preOpr == 'x') {
+        finalResult = mul();
+      } else if (preOpr == '/') {
+        finalResult = div();
+      }
+    } else if (btnText == '+' ||
+        btnText == '-' ||
+        btnText == 'x' ||
+        btnText == '/' ||
+        btnText == '=') {
+      if (numOne == 0) {
+        numOne = double.parse(result);
+      } else {
+        numTwo = double.parse(result);
+      }
+
+      if (opr == '+') {
+        finalResult = add();
+      } else if (opr == '-') {
+        finalResult = sub();
+      } else if (opr == 'x') {
+        finalResult = mul();
+      } else if (opr == '/') {
+        finalResult = div();
+      }
+      preOpr = opr;
+      opr = btnText;
+      result = '';
+    } else if (btnText == '%') {
+      result = numOne / 100;
+      finalResult = doesContainDecimal(result);
+    } else if (btnText == '.') {
+      if (!result.toString().contains('.')) {
+        result = result.toString() + '.';
+      }
+      finalResult = result;
+    } else if (btnText == '+/-') {
+      result.toString().startsWith('-')
+          ? result = result.toString().substring(1)
+          : result = '-' + result.toString();
+      finalResult = result;
+    } else {
+      result = result + btnText;
+      finalResult = result;
+    }
+
+    setState(() {
+      if (btnText == 'AC') {
+        runtime = '';
+      } else if (btnText == '+/-') {
+        runtime += ' (-/+) ';
+      } else {
+        runtime += btnText;
+      }
+      text = finalResult;
+    });
+  }
+
+  String add() {
+    result = (numOne + numTwo).toString();
+    numOne = double.parse(result);
+    return doesContainDecimal(result);
+  }
+
+  String sub() {
+    result = (numOne - numTwo).toString();
+    numOne = double.parse(result);
+    return doesContainDecimal(result);
+  }
+
+  String mul() {
+    result = (numOne * numTwo).toString();
+    numOne = double.parse(result);
+    return doesContainDecimal(result);
+  }
+
+  String div() {
+    result = (numOne / numTwo).toString();
+    numOne = double.parse(result);
+    return doesContainDecimal(result);
+  }
+
+  String doesContainDecimal(dynamic result) {
+    if (result.toString().contains('.')) {
+      List<String> splitDecimal = result.toString().split('.');
+      if (!(int.parse(splitDecimal[1]) > 0))
+        return result = splitDecimal[0].toString();
+    }
+    return result;
+  }
+
+  Widget calcBtn(String btnTxt) {
+    return Container(
+      width: 78.0,
+      height: 72.0,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        color: const Color(0xffffffff),
+        boxShadow: [
+          BoxShadow(
+            color: const Color(0x29000000),
+            offset: Offset(0, 0),
+            blurRadius: 12,
+          ),
+        ],
+      ),
+      child: RaisedButton(
+        onPressed: () {
+          calculation(btnTxt);
+        },
+        child: Text(
+          btnTxt,
+          style: TextStyle(fontSize: 35, color: Color(0xff6988dc)),
+        ),
+        color: Color(0xffffffff),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+          side: BorderSide(
+            color: Color(0xffffffff),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff6988dc),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 3),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            SingleChildScrollView(
+              scrollDirection: Axis.vertical,
+              child: Container(
+                margin: EdgeInsets.symmetric(horizontal: 30),
+                width: 347.0,
+                height: 252.0,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(45.0),
+                  color: const Color(0xffffffff),
+                  boxShadow: [
+                    BoxShadow(
+                      color: const Color(0x29000000),
+                      offset: Offset(0, 0),
+                      blurRadius: 27,
+                    ),
+                  ],
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.fromLTRB(0, 0, 0, 70),
+                        child: Text(
+                          '$runtime',
+                          style: TextStyle(
+                            color: Color(0xff6988dc),
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        child: Text(
+                          '$text',
+                          textAlign: TextAlign.right,
+                          style: TextStyle(
+                            color: Color(0xff6988dc),
+                            fontSize: 100,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                calcBtn('AC'),
+                calcBtn('+/-'),
+                calcBtn('%'),
+                calcBtn('/'),
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                calcBtn('7'),
+                calcBtn('8'),
+                calcBtn('9'),
+                calcBtn('x'),
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                calcBtn('4'),
+                calcBtn('5'),
+                calcBtn('6'),
+                calcBtn('-'),
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                calcBtn('1'),
+                calcBtn('2'),
+                calcBtn('3'),
+                calcBtn('+'),
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                //this is button Zero
+                RaisedButton(
+                  padding: EdgeInsets.fromLTRB(32, 20, 125, 20),
+                  onPressed: () {
+                    // calculation('0');
+                    calculation('0');
+                  },
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    side: BorderSide(
+                      color: Color(0xffffffff),
+                    ),
+                  ),
+                  child: Text(
+                    '0',
+                    style: TextStyle(
+                      fontSize: 35,
+                      color: Color(0xff6988dc),
+                    ),
+                  ),
+                  color: Color(0xffffffff),
+                ),
+                calcBtn('.'),
+                calcBtn('='),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
