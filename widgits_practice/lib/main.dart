dce289f86fa93bfe1e6dfbcfd6b3d439815ae5fb
jaffarabbas <54433038+jaffarabbas@users.noreply.github.com>
2021-09-30 21:30:19 +0500

    30/09/2021 21:30:19

--- widgits_practice/lib/main.dart
+++ widgits_practice/lib/main.dart
@@ -1,5 +1,16 @@
 import 'package:flutter/material.dart';
 
 void main(){
-  
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Hello world!"),
+    );
+  }
 }
