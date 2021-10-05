bc52adffae2427a5c8ba47698a55d6dc275655a6
jaffarabbas <gamaportal8@gmail.com>
2021-10-05 20:57:32 +0500

    05/10/2021 20:57:32

--- widgits_practice/lib/main.dart
+++ widgits_practice/lib/main.dart
@@ -19,6 +19,7 @@ class MyApp extends StatelessWidget {
       darkTheme: ThemeData(
         brightness: Brightness.dark,
       ),
+      initialRoute: "/login",
       routes: {
         "/": (context) => HomePage(),
         "/login": (context) => LoginPage()
