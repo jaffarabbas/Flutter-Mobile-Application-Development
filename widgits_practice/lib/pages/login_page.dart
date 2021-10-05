2846d1c61c0345c3ff525ea4d4f9371d17d4e2c6
jaffarabbas <gamaportal8@gmail.com>
2021-10-05 20:57:32 +0500

    05/10/2021 20:57:32

--- widgits_practice/lib/pages/login_page.dart
+++ widgits_practice/lib/pages/login_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
@@ -6,8 +8,15 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text('Login Page'),
+      color: Colors.white,
+      child: Column(
+        children: [
+          Image.asset("assets/images/login.png",fit: BoxFit.cover,),
+          Text('Welcome',style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),),
+        ],
       ),
     );
   }
