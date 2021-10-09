759a4ac5afa66796e9d1917cf9b4cea5d48705f7
jaffarabbas <gamaportal8@gmail.com>
2021-10-09 13:19:01 +0500

    09/10/2021 13:19:00

--- widgits_practice/lib/pages/login_page.dart
+++ widgits_practice/lib/pages/login_page.dart
@@ -9,14 +9,52 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
-      child: Column(
-        children: [
-          Image.asset("assets/images/login.png",fit: BoxFit.cover,),
-          Text('Welcome',style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-          ),),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/images/login.png",
+              fit: BoxFit.cover,
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            Text(
+              'Welcome',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                        hintText: 'Enter Username', labelText: 'Username'),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        hintText: 'Enter Password', labelText: 'Passowrd'),
+                  )
+                ],
+              ),
+            ),
+            ElevatedButton(
+              child: Text('Login here'),
+              style: TextButton.styleFrom(),
+              onPressed: () {},
+            )
+          ],
+        ),
       ),
     );
   }
