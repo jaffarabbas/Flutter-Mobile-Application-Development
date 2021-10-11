79b4ea4aa8caa2ebaacc0945f7734213fe293967
jaffarabbas <gamaportal8@gmail.com>
2021-10-11 10:00:42 +0500

    11/10/2021 10:00:42

--- login_app/lib/main.dart
+++ login_app/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
      return MaterialApp(
        debugShowCheckedModeBanner: false,
-       title: 'Login Page',
+       title: 'Login',
        theme: ThemeData(
          fontFamily: GoogleFonts.roboto().fontFamily,
        ),
