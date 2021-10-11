5a6dd229b7fe23c0b6f272152af19da883e7a0a4
jaffarabbas <gamaportal8@gmail.com>
2021-10-11 10:00:42 +0500

    11/10/2021 10:00:42

--- login_app/lib/containers/social_icon_set.dart
+++ login_app/lib/containers/social_icon_set.dart
@@ -4,33 +4,33 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class SocialIconSet extends StatelessWidget {
-  const SocialIconSet({ Key? key }) : super(key: key);
+  const SocialIconSet({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-            padding: EdgeInsets.only(top: 30),
-            width: 180,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                FaIcon(
-                  FontAwesomeIcons.facebook,
-                  color: Color(0xFFFC5D2B),
-                  size: 30,
-                ),
-                FaIcon(
-                  FontAwesomeIcons.google,
-                  color: Color(0xFFFC5D2B),
-                  size: 30,
-                ),
-                FaIcon(
-                  FontAwesomeIcons.github,
-                  color: Color(0xFFFC5D2B),
-                  size: 30,
-                ),
-              ],
-            ),
-          );
+      padding: EdgeInsets.only(top: 30),
+      width: 180,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          FaIcon(
+            FontAwesomeIcons.facebook,
+            color: Color(0xFFFC5D2B),
+            size: 30,
+          ),
+          FaIcon(
+            FontAwesomeIcons.google,
+            color: Color(0xFFFC5D2B),
+            size: 30,
+          ),
+          FaIcon(
+            FontAwesomeIcons.github,
+            color: Color(0xFFFC5D2B),
+            size: 30,
+          ),
+        ],
+      ),
+    );
   }
 }
