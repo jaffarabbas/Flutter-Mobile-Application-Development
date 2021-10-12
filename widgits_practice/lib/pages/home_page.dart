9043bcfec42f8f98daf61a34ae70f02cfc763dbd
jaffarabbas <gamaportal8@gmail.com>
2021-10-12 23:20:03 +0500

    12/10/2021 23:20:03

--- widgits_practice/lib/pages/home_page.dart
+++ widgits_practice/lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:widgits_practice/widgits/drawer.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -18,12 +19,12 @@ class HomePage extends StatelessWidget {
               style: TextStyle(
                 fontSize: 30,
                 fontWeight: FontWeight.bold,
-                color: Colors.cyan
+                color: Colors.deepPurple
               ),
             ),
           ),
         ),
-        drawer: Drawer(),
+        drawer: AppDrawer(),
       );
   }
 }
