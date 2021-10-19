7f22df48ede1e861585a04b4085d0854140d9b4a
jaffarabbas <gamaportal8@gmail.com>
2021-10-19 12:03:50 +0500

    19/10/2021 12:03:50

--- whatsapp_clone/lib/dataSource/dataMap.dart
+++ whatsapp_clone/lib/dataSource/dataMap.dart
@@ -3,42 +3,7 @@
 class DataSource {
   static Map<dynamic, dynamic> Datamap() {
     return {
-      "friendsList": {
-        0: {
-          "firendId": 1,
-          "image": "https://avatars.githubusercontent.com/u/54433038?v=4",
-          "username": "Jaffar",
-        },
-        1: {
-          "firendId": 2,
-          "image": "https://avatars.githubusercontent.com/u/54433038?v=4",
-          "username": "Ahmed",
-        }
-      },
-      "chatList": {
-        1: {
-          "mainUser": {
-            "firendId": 1,
-            "image": "",
-            "username": "gfg",
-          },
-          "chatUser": {
-            "firendId": 2,
-            "image": "",
-            "username": "",
-          },
-          "messsges": {
-            "mainUser+messageid": {
-              "message": "",
-              "timestamps": "",
-            },
-            "chatUser+messageid": {
-              "message": "",
-              "timestamps": "",
-            }
-          }
-        }
-      }
+      "friendsList":{}
     };
   }
 }
