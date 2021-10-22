f8934ff2c94c7ff37210fc9dcdf6cc6f4469a2d3
jaffarabbas <gamaportal8@gmail.com>
2021-10-22 20:17:37 +0500

    22/10/2021 20:17:37

---
+++ widgits_practice/lib/widgits/themes.dart
@@ -0,0 +1,23 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppTheme {
+  static ThemeData lightTheme(BuildContext context) => ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.roboto().fontFamily,
+        appBarTheme: AppBarTheme(
+          color: Colors.white,
+          elevation: 0.0,
+          iconTheme: IconThemeData(
+            color: Colors.black,
+          ),
+          toolbarTextStyle: Theme.of(context).textTheme.bodyText2,
+          titleTextStyle: Theme.of(context).textTheme.headline6,
+        ),
+      );
+  static ThemeData darkTheme(BuildContext context) => ThemeData(
+    brightness: Brightness.dark,
+  );
+}
