1982380e1ca02b144612fda2530d58f682e73a2c
jaffarabbas <gamaportal8@gmail.com>
2021-10-23 07:57:20 +0500

    23/10/2021 07:57:15

---
+++ widgits_practice/lib/models/catalog.dart
@@ -0,0 +1,10 @@
+class ItemModel{
+  final String id;
+  final String name;
+  final String description;
+  final num price;
+  final String color;
+  final String imageUrl;
+
+  ItemModel({required this.id, required this.name,required this.description,required this.price,required this.color,required this.imageUrl});
+}
