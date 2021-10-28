52e8674104dfb6cf0a562ed84d798aeaeb64138d
jaffarabbas <gamaportal8@gmail.com>
2021-10-28 14:38:07 +0500

    28/10/2021 14:38:07

--- widgits_practice/lib/widgits/themes.dart
+++ widgits_practice/lib/widgits/themes.dart
@@ -6,7 +6,7 @@ import 'package:google_fonts/google_fonts.dart';
 class AppTheme {
   static ThemeData lightTheme(BuildContext context) => ThemeData(
         primarySwatch: Colors.deepPurple,
-        fontFamily: GoogleFonts.roboto().fontFamily,
+        fontFamily: GoogleFonts.poppins().fontFamily,
         appBarTheme: AppBarTheme(
           color: Colors.white,
           elevation: 0.0,
@@ -21,4 +21,8 @@ class AppTheme {
   static ThemeData darkTheme(BuildContext context) => ThemeData(
     brightness: Brightness.dark,
   );
+
+  //colors
+  static Color creamColor = Color(0xfff5f5f5);
+  static Color darkBluishColor = Color(0xff403b58);
 }
