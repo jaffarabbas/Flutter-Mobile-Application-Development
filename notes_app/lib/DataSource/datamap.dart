8cc3c4418eabb4545bf264f97f7cef72b7682636
jaffarabbas <gamaportal8@gmail.com>
2021-11-07 20:55:07 +0500

    07/11/2021 20:55:07

---
+++ notes_app/lib/DataSource/datamap.dart
@@ -0,0 +1,45 @@
+// ignore_for_file: non_constant_identifier_names
+
+class Datamap {
+  static DataSource() {
+    return {
+      "todo": [
+        {
+          'heading': 'How to make your oesonal brand stand out online',
+          'description':
+              'How to make your oesonal brand stand out onlineHow to make your oesonal brand stand out onlineHow to make your oesonal brand stand out onlineHow to make your oesonal brand stand out onlineHow to make your oesonal brand stand out onlineHow to make your oesonal brand stand out online',
+          'timestamp': '1-jan-2021',
+          'isOne': true,
+        },
+        {
+          'heading': 'How to make your oesonal brand stand out online',
+          'description':
+              'How to make your oesonal brand stand out onlineHow to make your oesonal brand stand out onlineHow to make your oesonal brand stand out onlineHow to make your oesonal brand stand out onlineHow to make your oesonal brand stand out onlineHow to make your oesonal brand stand out online',
+          'timestamp': '1-jan-2021',
+          'isOne': true,
+        },
+        {
+          'heading': 'How to make your oesonal brand stand out online',
+          'description':
+              'How to make your oesonal brand stand out onlineHow to make your oesonal brand stand out onlineHow to make your oesonal brand stand out onlineHow to make your oesonal brand stand out onlineHow to make your oesonal brand stand out onlineHow to make your oesonal brand stand out online',
+          'timestamp': '1-jan-2021',
+          'isOne': false,
+        },
+        {
+          'heading': 'How to make your oesonal brand stand out online',
+          'description':
+              'How to make your oesonal brand stand out onlineHow to make your oesonal brand stand out onlineHow to make your oesonal brand stand out onlineHow to make your oesonal brand stand out onlineHow to make your oesonal brand stand out onlineHow to make your oesonal brand stand out online',
+          'timestamp': '1-jan-2021',
+          'isOne': false,
+        },
+        {
+          'heading': 'How to make your oesonal brand stand out online',
+          'description':
+              'How to make your oesonal brand stand out onlineHow to make your oesonal brand stand out onlineHow to make your oesonal brand stand out onlineHow to make your oesonal brand stand out onlineHow to make your oesonal brand stand out onlineHow to make your oesonal brand stand out online',
+          'timestamp': '1-jan-2021',
+          'isOne': false,
+        },
+      ],
+    };
+  }
+}
