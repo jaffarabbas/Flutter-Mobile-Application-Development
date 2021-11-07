725b87486e37ab870255a5d70a06435e89b3bb56
jaffarabbas <gamaportal8@gmail.com>
2021-11-07 20:55:08 +0500

    07/11/2021 20:55:07

---
+++ notes_app/lib/widgits/themes.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppTheme{
+  static const Color primarybackground = Color(0xff252525);
+  static const Color iconbackground = Color(0xff3B3B3B);
+  static const Color dateColor = Color(0xff676767);
+  static const Color headingFontColor = Color(0xffffffff);
+  static const Color paragraphColor = Color(0xffDFDCDC);
+  static const Color noteColor1 = Color(0xffFFA68B);
+  static const Color noteColor2 = Color(0xffFFD179);
+  static const Color noteColor3 = Color(0xffE1F898);
+  static const Color noteColor4 = Color(0xff67E1ED);
+  static const Color noteColor5 = Color(0xffDC7CDA);
+  static const Color noteColor6 = Color(0xffFF7BAE);
+}
