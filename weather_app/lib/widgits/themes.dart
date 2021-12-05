3eb83ccf01560ff8c7f8e62e8ec5f8665afa3292
Jaffar Abbas <gamaportal8@gmail.com>
2021-12-05 07:56:52 +0500

    05/12/2021 07:56:30

---
+++ weather_app/lib/widgits/themes.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppTheme{
+  static const Color primarybackground = Color(0xff1590F6);
+  static const Color secondarybackground = Color(0xff000A18);
+  static const Color headingFontColor = Color(0xffffffff);
+}
