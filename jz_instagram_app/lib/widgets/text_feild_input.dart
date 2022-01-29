c64b16e75f60708097a80932ee780c02a08699f6
Jaffar Abbas <gamaportal8@gmail.com>
2022-01-29 08:04:37 +0500

    29/01/2022 08:04:13

---
+++ jz_instagram_app/lib/widgets/text_feild_input.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class TextFeildInput extends StatelessWidget {
+  final TextEditingController textEditingController;
+  final bool isPassword;
+  final String hintText;
+  final TextInputType textInputType;
+  const TextFeildInput(
+      {Key? key,
+      required this.textEditingController,
+      this.isPassword = false,
+      required this.hintText,
+      required this.textInputType})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final inputBorder =
+        OutlineInputBorder(borderSide: Divider.createBorderSide(context));
+    return TextField(
+      controller: textEditingController,
+      decoration: InputDecoration(
+        hintText: hintText,
+        border: inputBorder,
+        focusedBorder: inputBorder,
+        enabledBorder: inputBorder,
+        filled: true,
+        contentPadding: const EdgeInsets.all(8),
+      ),
+      keyboardType: textInputType,
+      obscureText: isPassword,
+    );
+  }
+}
