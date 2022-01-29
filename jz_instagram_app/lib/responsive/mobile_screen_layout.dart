8e5f4a8a4863126c2efe6342e017b7675e430c5b
Jaffar Abbas <gamaportal8@gmail.com>
2022-01-29 08:04:27 +0500

    29/01/2022 08:04:13

---
+++ jz_instagram_app/lib/responsive/mobile_screen_layout.dart
@@ -0,0 +1,14 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class MobileScreenLayout extends StatelessWidget {
+  const MobileScreenLayout({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Text('this is mobile screen'),
+    );
+  }
+}
