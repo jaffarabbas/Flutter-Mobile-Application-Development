f9d07d0b27d7992e19ffd34679b078c10c8886e9
Jaffar Abbas <gamaportal8@gmail.com>
2022-01-29 08:04:28 +0500

    29/01/2022 08:04:13

---
+++ jz_instagram_app/lib/responsive/web_screen_layout.dart
@@ -0,0 +1,14 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class WebScreenLayout extends StatelessWidget {
+  const WebScreenLayout({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Text('This is Web screen'),
+    );
+  }
+}
