e691841342ab50e188eaafd7e70a288c5c5a4fe8
Jaffar Abbas <gamaportal8@gmail.com>
2022-02-04 16:44:33 +0500

    04/02/2022 16:44:30

--- jz_instagram_app/lib/utils/utils.dart
+++ jz_instagram_app/lib/utils/utils.dart
@@ -4,8 +4,16 @@ import 'package:image_picker/image_picker.dart';
 PickImage(ImageSource source) async {
   final ImagePicker _imagePicker = ImagePicker();
   XFile? _file = await _imagePicker.pickImage(source: source);
-  if(_file!=null) {
+  if (_file != null) {
     return _file.readAsBytes();
   }
   print('No image');
+}
+
+ShowSnakBar(String content, BuildContext context) {
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      content: Text(content),
+    ),
+  );
 }
