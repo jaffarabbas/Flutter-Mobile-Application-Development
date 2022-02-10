56ba7cce40dcd6c2ee163d165ac1be60f1c549f0
Jaffar Abbas <gamaportal8@gmail.com>
2022-02-10 22:16:28 +0500

    10/02/2022 22:16:25

--- flutter_django_api_connection/lib/widgits/userList.dart
+++ flutter_django_api_connection/lib/widgits/userList.dart
@@ -14,16 +14,35 @@ class _UserListState extends State<UserList> {
   Widget build(BuildContext context) {
     return ListView.builder(
       shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
       itemCount: widget.itemCount,
       itemBuilder: (context, index) {
         return Card(
-          child: ListTile(
-            title: Text(
-              widget.snapshot[index].name
-            ),
-            subtitle: Text(
-              widget.snapshot[index].city
-            ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                width: 250,
+                child: ListTile(
+                  title: Text(
+                    '${widget.snapshot[index].id} ${widget.snapshot[index].name} ROll : ${widget.snapshot[index].roll}'
+                  ),
+                  subtitle: Text(
+                    widget.snapshot[index].city
+                  ),
+                ),
+              ),
+              Container(
+                width: 100,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    IconButton(onPressed: (){}, icon: Icon(Icons.mode_edit_rounded,color: Colors.blueAccent,)),
+                    IconButton(onPressed: (){}, icon: Icon(Icons.delete,color: Colors.blueAccent,)),
+                  ],
+                ),
+              )
+            ],
           ),
         );
       },
