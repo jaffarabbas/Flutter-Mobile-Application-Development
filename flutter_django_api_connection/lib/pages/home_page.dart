57317e9e697d99b9f8d5f436272411b60dced3a2
Jaffar Abbas <gamaportal8@gmail.com>
2022-02-10 22:16:27 +0500

    10/02/2022 22:16:25

--- flutter_django_api_connection/lib/pages/home_page.dart
+++ flutter_django_api_connection/lib/pages/home_page.dart
@@ -23,6 +23,34 @@ class _HomePageState extends State<HomePage> {
     return dataSource;
   }
 
+  Future<void> _showMyDialog() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Insert Student Data'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: const <Widget>[
+                InsertData()
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('cancel'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,32 +59,30 @@ class _HomePageState extends State<HomePage> {
       ),
       body: SingleChildScrollView(
         child: Container(
-          child: Column(
-            children: [
-              InsertData(),
-              Container(
-                child: FutureBuilder(
-                  future: getStudent(),
-                  builder: (BuildContext context, AsyncSnapshot snapshot) {
-                    if (snapshot.hasData) {
-                      return UserList(
-                          itemCount: snapshot.data.length,
-                          snapshot: snapshot.data,
-                      );
-                    } else if (snapshot.hasError) {
-                      print(getStudent());
-                      return Text('${snapshot.error}');
-                    }
-                    return Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  },
-                ),
-              ),
-            ],
+          child: FutureBuilder(
+            future: getStudent(),
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              if (snapshot.hasData) {
+                return UserList(
+                    itemCount: snapshot.data.length,
+                    snapshot: snapshot.data,
+                );
+              } else if (snapshot.hasError) {
+                print(getStudent());
+                return Text('${snapshot.error}');
+              }
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            },
           ),
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+      onPressed: _showMyDialog,
+      child: Icon(Icons.add),
+      backgroundColor: Colors.blueAccent,
+    ),
     );
   }
 }
