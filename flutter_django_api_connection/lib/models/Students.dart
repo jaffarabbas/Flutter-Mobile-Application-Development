0487e15fda2f07f7c610b097daca87a9b0b7b355
Jaffar Abbas <gamaportal8@gmail.com>
2022-02-10 22:16:26 +0500

    10/02/2022 22:16:25

--- flutter_django_api_connection/lib/models/Students.dart
+++ flutter_django_api_connection/lib/models/Students.dart
@@ -1,12 +1,14 @@
 // ignore_for_file: avoid_function_literals_in_foreach_calls
 class Students{
+  late int id;
   late String name;
   late int roll;
   late String city;
   //assiging constructor parameter
-  Students({required this.name,required this.roll,required this.city});
+  Students({required this.id,required this.name,required this.roll,required this.city});
   //
   Students.fromJson(Map<String,dynamic> json){
+    id = json['id'];
     name = json['name'];
     roll = json['roll'];
     city = json['city'];
