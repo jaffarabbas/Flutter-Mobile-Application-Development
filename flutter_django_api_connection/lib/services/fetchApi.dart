c61567945eccf5f06bd13380950eec104f84d4bf
Jaffar Abbas <gamaportal8@gmail.com>
2022-02-10 22:16:27 +0500

    10/02/2022 22:16:25

--- flutter_django_api_connection/lib/services/fetchApi.dart
+++ flutter_django_api_connection/lib/services/fetchApi.dart
@@ -8,7 +8,7 @@ import 'package:http/http.dart' as http;
 class ApiService {
   Future<List<Students>> fetchData() async {
     final response = await http
-        .get(Uri.parse("https://jzstudentinfo.herokuapp.com/getstudent/"));
+        .get(Uri.parse(Links.get));
     if (response.statusCode == 200) {
       return Students.fromJsonList(jsonDecode(response.body));
     } else {
@@ -16,24 +16,11 @@ class ApiService {
     }
   }
 
-  Future<bool> postStudent(String name, int roll, String city) async {
+  Future<void> postStudent(String name, int roll, String city) async {
     final response = await http.post(
-      Uri.parse('https://jzstudentinfo.herokuapp.com/poststudent/'),
-      // headers: <String, String>{
-      //   'Content-Type': 'application/json; charset=UTF-8',
-      // },
+      Uri.parse(Links.post),
       body: jsonEncode(
           <String, dynamic>{'name': name, 'roll': roll, 'city': city}),
     );
-
-    if (response.statusCode == 201) {
-      // If the server did return a 201 CREATED response,
-      // then parse the JSON.
-      return true;
-    } else {
-      // If the server did not return a 201 CREATED response,
-      // then throw an exception.
-      throw Exception('Failed to create album.');
-    }
   }
 }
