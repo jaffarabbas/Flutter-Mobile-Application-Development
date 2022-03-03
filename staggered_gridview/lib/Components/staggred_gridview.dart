6e6ca2965dcae8532bd0d68ea49523f3f77f989c
Jaffar Abbas <gamaportal8@gmail.com>
2022-03-03 18:00:34 +0500

    03/03/2022 18:00:32

--- staggered_gridview/lib/Components/staggred_gridview.dart
+++ staggered_gridview/lib/Components/staggred_gridview.dart
@@ -2,97 +2,107 @@ import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:staggered_gridview/widgets/grid_container.dart';
 
-class StaggeredGridView extends StatelessWidget {
-  const StaggeredGridView({Key? key}) : super(key: key);
+class StaggeredGridView extends StatefulWidget {
+  StaggeredGridView({Key? key}) : super(key: key);
 
   @override
+  State<StaggeredGridView> createState() => _StaggeredGridViewState();
+}
+
+class _StaggeredGridViewState extends State<StaggeredGridView> {
+  @override
+  var twoCount = 1;
+  var threeCount = 0;
   Widget build(BuildContext context) {
+    int indexIncreatement() {
+      int result = 0;
+      if (twoCount != 2) {
+        result = 2;
+        setState(() {
+          twoCount++;
+        });
+      } else if (threeCount != 2) {
+        result = 3;
+        setState(() {
+          threeCount++;
+        });
+        if (threeCount == 2 && twoCount == 2) {
+          setState(() {
+            twoCount = 0;
+            threeCount = 0;
+          });
+        }
+      }
+      return result;
+    }
+
+    List<Widget> stg = [];
+
+    // int size = stg.indexOf(element);
+
+    stg = [
+      StaggeredGridTile.count(
+        crossAxisCellCount: 2,
+        mainAxisCellCount: indexIncreatement(),
+        child: GridContainer(color: Colors.amber, text: "1"),
+      ),
+      StaggeredGridTile.count(
+        crossAxisCellCount: 2,
+        mainAxisCellCount: indexIncreatement(),
+        child: GridContainer(color: Colors.red, text: "2"),
+      ),
+      StaggeredGridTile.count(
+        crossAxisCellCount: 2,
+        mainAxisCellCount: indexIncreatement(),
+        child: GridContainer(color: Colors.blueAccent, text: "3"),
+      ),
+      StaggeredGridTile.count(
+        crossAxisCellCount: 2,
+        mainAxisCellCount: indexIncreatement(),
+        child: GridContainer(color: Colors.pinkAccent, text: "4"),
+      ),
+      StaggeredGridTile.count(
+        crossAxisCellCount: 2,
+        mainAxisCellCount: indexIncreatement(),
+        child: GridContainer(color: Colors.pinkAccent, text: "4"),
+      ),
+      StaggeredGridTile.count(
+        crossAxisCellCount: 2,
+        mainAxisCellCount: indexIncreatement(),
+        child: GridContainer(color: Colors.amber, text: "1"),
+      ),
+      StaggeredGridTile.count(
+        crossAxisCellCount: 2,
+        mainAxisCellCount: indexIncreatement(),
+        child: GridContainer(color: Colors.red, text: "2"),
+      ),
+      StaggeredGridTile.count(
+        crossAxisCellCount: 2,
+        mainAxisCellCount: indexIncreatement(),
+        child: GridContainer(color: Colors.blueAccent, text: "3"),
+      ),
+      StaggeredGridTile.count(
+        crossAxisCellCount: 2,
+        mainAxisCellCount: indexIncreatement(),
+        child: GridContainer(color: Colors.pinkAccent, text: "4"),
+      ),
+      StaggeredGridTile.count(
+        crossAxisCellCount: 2,
+        mainAxisCellCount: indexIncreatement(),
+        child: GridContainer(color: Colors.blueAccent, text: "3"),
+      ),
+      StaggeredGridTile.count(
+        crossAxisCellCount: 2,
+        mainAxisCellCount: indexIncreatement(),
+        child: GridContainer(color: Colors.pinkAccent, text: "4"),
+      ),
+    ];
+
     return StaggeredGrid.count(
       crossAxisCount: 4,
-      mainAxisSpacing: 10,
-      crossAxisSpacing: 4,
-      children: [
-        StaggeredGridTile.count(
-          crossAxisCellCount: 2,
-          mainAxisCellCount: 2,
-          child: GridContainer(color: Colors.amber, text: "1"),
-        ),
-        StaggeredGridTile.count(
-          crossAxisCellCount: 2,
-          mainAxisCellCount: 3,
-          child: GridContainer(color: Colors.red, text: "2"),
-        ),
-        StaggeredGridTile.count(
-          crossAxisCellCount: 2,
-          mainAxisCellCount: 3,
-          child: GridContainer(color: Colors.blueAccent, text: "3"),
-        ),
-        StaggeredGridTile.count(
-          crossAxisCellCount: 2,
-          mainAxisCellCount: 2,
-          child: GridContainer(color: Colors.pinkAccent, text: "4"),
-        ),
-        StaggeredGridTile.count(
-          crossAxisCellCount: 2,
-          mainAxisCellCount: 2,
-          child: GridContainer(color: Colors.amber, text: "1"),
-        ),
-        StaggeredGridTile.count(
-          crossAxisCellCount: 2,
-          mainAxisCellCount: 3,
-          child: GridContainer(color: Colors.red, text: "2"),
-        ),
-        StaggeredGridTile.count(
-          crossAxisCellCount: 2,
-          mainAxisCellCount: 3,
-          child: GridContainer(color: Colors.blueAccent, text: "3"),
-        ),
-        StaggeredGridTile.count(
-          crossAxisCellCount: 2,
-          mainAxisCellCount: 2,
-          child: GridContainer(color: Colors.pinkAccent, text: "4"),
-        ),
-        StaggeredGridTile.count(
-          crossAxisCellCount: 2,
-          mainAxisCellCount: 2,
-          child: GridContainer(color: Colors.amber, text: "1"),
-        ),
-        StaggeredGridTile.count(
-          crossAxisCellCount: 2,
-          mainAxisCellCount: 3,
-          child: GridContainer(color: Colors.red, text: "2"),
-        ),
-        StaggeredGridTile.count(
-          crossAxisCellCount: 2,
-          mainAxisCellCount: 3,
-          child: GridContainer(color: Colors.blueAccent, text: "3"),
-        ),
-        StaggeredGridTile.count(
-          crossAxisCellCount: 2,
-          mainAxisCellCount: 2,
-          child: GridContainer(color: Colors.pinkAccent, text: "4"),
-        ),
-        // StaggeredGridTile.count(
-        //   crossAxisCellCount: 2,
-        //   mainAxisCellCount: 1,
-        //   child: Tile(index: 1),
-        // ),
-        // StaggeredGridTile.count(
-        //   crossAxisCellCount: 1,
-        //   mainAxisCellCount: 1,
-        //   child: Tile(index: 2),
-        // ),
-        // StaggeredGridTile.count(
-        //   crossAxisCellCount: 1,
-        //   mainAxisCellCount: 1,
-        //   child: Tile(index: 3),
-        // ),
-        // StaggeredGridTile.count(
-        //   crossAxisCellCount: 4,
-        //   mainAxisCellCount: 2,
-        //   child: Tile(index: 4),
-        // ),
-      ],
+      mainAxisSpacing: 20,
+      crossAxisSpacing: 30,
+      children: stg,
     );
   }
 }
