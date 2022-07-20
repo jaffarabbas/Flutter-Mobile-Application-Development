773351baa02626e047b3207f7b2a31fd307de290
Jaffar Abbas <gamaportal8@gmail.com>
2022-07-20 21:14:23 +0500

    Updated lib/pages/home_page.dart at 20/07/2022 21:14:23

--- git_test_application/lib/pages/home_page.dart
+++ git_test_application/lib/pages/home_page.dart
@@ -2,11 +2,12 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:git_test_application/pages/commit_page.dart';
+import 'package:git_test_application/pages/test.dart';
 import 'package:path/path.dart' as p;
 import 'package:process_run/shell.dart';
 import 'dart:io' as io;
-class HomePage extends StatefulWidget {
 
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
@@ -16,7 +17,34 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    TextEditingController controller = TextEditingController();
+    List<String> fileForCommit = [];
     var myDir = Directory('J:\\Github\\');
+
+    Future<void> GetCommitDetails(String path) async {
+      var shell = Shell();
+      shell = shell.pushd(path);
+      await shell.run('''
+
+      # Display some text
+      cd
+      git status --porcelain
+      ''').then((value) => {
+            setState(() {
+              controller.text = value.outText;
+            }),
+          });
+    }
+
+    Future<List<String>> CommitDataList(List data) async {
+      // GetCommitDetails();
+      for (var element in data) {
+        fileForCommit.add(element.toString().split(" ").last);
+      }
+      //removing file url from the list
+      fileForCommit.removeAt(0);
+      return fileForCommit;
+    }
 
     return Scaffold(
       body: Container(
@@ -29,18 +57,24 @@ class _HomePageState extends State<HomePage> {
                   itemCount: snapshot.data!.length,
                   itemBuilder: (context, index) {
                     return ListTile(
-                      title: InkWell(
-                        onTap: (){
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => CommitPage(path: snapshot.data![index].path),
+                        title: InkWell(
+                      onTap: () async {
+                        GetCommitDetails(snapshot.data![index].path).then(
+                            (value) =>
+                                {CommitDataList(controller.text.split("\n"))});
+                        // print(fileForCommit);
+                       Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => CommitPage(
+                              path: snapshot.data![index].path,
+                              data: fileForCommit,
                             ),
-                          );
-                        },
-                        child: Text(snapshot.data![index].path),
-                      )
-                    );
+                          ),
+                        );
+                      },
+                      child: Text(snapshot.data![index].path),
+                    ));
                   },
                 );
               } else if (snapshot.hasError) {
