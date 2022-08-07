22e35b263c74ec77578f531b74d6f9f63a4e5be4
Jaffar Abbas <gamaportal8@gmail.com>
2022-08-07 22:26:05 +0500

    Commited File : main.dart at 8/7/2022 10:26:04 PM

--- flutter_map_integration/lib/main.dart
+++ flutter_map_integration/lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: PolyLineIntegration(),
+      home: HomePage(),
     );
   }
 }
