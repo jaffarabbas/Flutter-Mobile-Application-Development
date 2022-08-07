8751446bd2126424435c1ba0a9e1f9f884c0bfe5
Jaffar Abbas <gamaportal8@gmail.com>
2022-08-07 14:46:47 +0500

    Commited File : home_page.dart at 8/7/2022 2:46:46 PM

--- flutter_map_integration/lib/pages/home_page.dart
+++ flutter_map_integration/lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:typed_data';
 import 'dart:ui' as ui;
+import 'package:custom_info_window/custom_info_window.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/src/foundation/key.dart';
@@ -16,11 +17,13 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  CustomInfoWindowController _customInfoWindowController =
+      CustomInfoWindowController();
   Completer<GoogleMapController> _controller = Completer();
   MapType _currentMapType = MapType.normal;
 
   static const CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(24.8959986,67.0300502),
+    target: LatLng(24.8959986, 67.0300502),
     zoom: 14.4746,
   );
 
@@ -35,12 +38,16 @@ class _HomePageState extends State<HomePage> {
     // ),
   ];
   List<Marker> _markers = [];
-  List<String> images = ["assets/images/car.png", "assets/images/food-delivery.png", "assets/images/walk.png"];
+  List<String> images = [
+    "assets/images/car.png",
+    "assets/images/food-delivery.png",
+    "assets/images/walk.png"
+  ];
 
   List<LatLng> _latLng = [
     LatLng(24.860966, 66.990501),
     LatLng(24.871940, 66.988060),
-    LatLng(24.8959986,67.0300502),
+    LatLng(24.8959986, 67.0300502),
   ];
 
   void goToHardCodedLocation() async {
@@ -63,18 +70,18 @@ class _HomePageState extends State<HomePage> {
   }
 
   void navigateToCurrentLocation() async {
-    final Uint8List markerIcon = await getBytesFromAssets(images[2], 100);
+    print("NNNNNNNNNNNNNNNNNNNNNNNNNNN");
     getCurrentLocation().then((value) async {
-      _markers.add(
-        Marker(
-          icon: BitmapDescriptor.fromBytes(markerIcon),
-          markerId: MarkerId("69"),
-          position: LatLng(value.latitude, value.longitude),
-          infoWindow: InfoWindow(
-              title: 'My location',
-              snippet: "Lat: ${value.latitude} Long: ${value.longitude}"),
-        ),
-      );
+      // _markers.add(
+      //   Marker(
+      //     icon: BitmapDescriptor.fromBytes(markerIcon),
+      //     markerId: MarkerId("69"),
+      //     position: LatLng(value.latitude, value.longitude),
+      //     infoWindow: InfoWindow(
+      //         title: 'My location',
+      //         snippet: "Lat: ${value.latitude} Long: ${value.longitude}"),
+      //   ),
+      // );
 
       CameraPosition cameraPosition = CameraPosition(
         target: LatLng(value.latitude, value.longitude),
@@ -102,11 +109,14 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
-  Future<Uint8List> getBytesFromAssets(String path,int width) async {
+  Future<Uint8List> getBytesFromAssets(String path, int width) async {
     ByteData data = await rootBundle.load(path);
-    ui.Codec codec = await ui.instantiateImageCodec(data.buffer.asUint8List(), targetWidth: width);
+    ui.Codec codec = await ui.instantiateImageCodec(data.buffer.asUint8List(),
+        targetWidth: width);
     ui.FrameInfo frameInfo = await codec.getNextFrame();
-    return (await frameInfo.image.toByteData(format: ui.ImageByteFormat.png))!.buffer.asUint8List(); 
+    return (await frameInfo.image.toByteData(format: ui.ImageByteFormat.png))!
+        .buffer
+        .asUint8List();
   }
 
   @override
@@ -118,21 +128,90 @@ class _HomePageState extends State<HomePage> {
     navigateToCurrentLocation();
   }
 
-  void loadData() async{
-    for(int i = 0; i < images.length; i++){
-      print(images[i]);
-       final Uint8List markerIcon = await getBytesFromAssets(images[i], 100);
+  void loadData() async {
+    for (int i = 0; i < images.length; i++) {
+      final Uint8List markerIcon = await getBytesFromAssets(images[i], 100);
       _markers.add(
         Marker(
           markerId: MarkerId(i.toString()),
           position: _latLng[i],
           infoWindow: InfoWindow(title: "Marker $i"),
           icon: BitmapDescriptor.fromBytes(markerIcon),
+          onTap: () {
+            _customInfoWindowController.addInfoWindow!(
+              Container(
+                height: 300,
+                width: 200,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      height: 100,
+                      width: 300,
+                      decoration: BoxDecoration(
+                        image: const DecorationImage(
+                          image: NetworkImage(
+                            'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+                          ),
+                          fit: BoxFit.fitWidth,
+                        ),
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    Container(
+                      width: 300,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              'Cat Shop',
+                              style: TextStyle(fontSize: 20),
+                            ),
+                            Text(
+                              'open',
+                              style: TextStyle(fontSize: 20),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: 300,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Description',
+                              style: TextStyle(fontSize: 15),
+                            ),
+                            Text(
+                              'This is a cat shop with it food',
+                              style: TextStyle(fontSize: 15),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              _latLng[i],
+            );
+          },
         ),
       );
-      setState(() {
-        
-      });
+      setState(() {});
     }
   }
 
@@ -176,17 +255,34 @@ class _HomePageState extends State<HomePage> {
         ],
       ),
       body: SafeArea(
-        child: GoogleMap(
-          initialCameraPosition: _kGooglePlex,
-          mapType: _currentMapType,
-          markers: Set<Marker>.of(_markers),
-          myLocationButtonEnabled: true,
-          myLocationEnabled: true,
-          // mapToolbarEnabled: true,
-          // compassEnabled: true,
-          onMapCreated: (GoogleMapController controller) {
-            _controller.complete(controller);
-          },
+        child: Stack(
+          children: [
+            GoogleMap(
+              initialCameraPosition: _kGooglePlex,
+              mapType: _currentMapType,
+              markers: Set<Marker>.of(_markers),
+              onTap: (position) {
+                _customInfoWindowController.hideInfoWindow!();
+              },
+              onCameraMove: (position) {
+                _customInfoWindowController.onCameraMove!();
+              },
+              myLocationButtonEnabled: true,
+              myLocationEnabled: true,
+              // mapToolbarEnabled: true,
+              // compassEnabled: true,
+              onMapCreated: (GoogleMapController controller) {
+                _customInfoWindowController.googleMapController = controller;
+                _controller.complete(controller);
+              },
+            ),
+            CustomInfoWindow(
+              controller: _customInfoWindowController,
+              height: 200,
+              width: 300,
+              offset: 35,
+            ),
+          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
