d3b649581786b4294b40687bdeba52195e7639a2
Jaffar Abbas <gamaportal8@gmail.com>
2022-08-07 14:46:46 +0500

    Commited File : main.dart at 8/7/2022 2:46:45 PM

--- flutter_map_integration/lib/main.dart
+++ flutter_map_integration/lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:custom_info_window/custom_info_window.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map_integration/components/convertLatLangToAddress.dart';
 import 'package:flutter_map_integration/components/custom_marker.dart';
+import 'package:flutter_map_integration/components/custome_Info_window.dart';
 import 'package:flutter_map_integration/components/google_places_api.dart';
 import 'package:flutter_map_integration/pages/home_page.dart';
 
