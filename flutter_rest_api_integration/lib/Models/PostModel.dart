bdb6662ee9d24809db153f1c8e3ae0aa5f91dbb7
Jaffar Abbas <gamaportal8@gmail.com>
2022-08-11 13:39:36 +0500

    Commited File : .gitignore at 8/11/2022 1:39:35 PM

---
+++ flutter_rest_api_integration/lib/Models/PostModel.dart
@@ -0,0 +1,51 @@
+/// userId : 1
+/// id : 1
+/// title : "sunt aut facere repellat provident occaecati excepturi optio reprehenderit"
+/// body : "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+
+class PostModel {
+  PostModel({
+      num? userId, 
+      num? id, 
+      String? title, 
+      String? body,}){
+    _userId = userId;
+    _id = id;
+    _title = title;
+    _body = body;
+}
+
+  PostModel.fromJson(dynamic json) {
+    _userId = json['userId'];
+    _id = json['id'];
+    _title = json['title'];
+    _body = json['body'];
+  }
+  num? _userId;
+  num? _id;
+  String? _title;
+  String? _body;
+PostModel copyWith({  num? userId,
+  num? id,
+  String? title,
+  String? body,
+}) => PostModel(  userId: userId ?? _userId,
+  id: id ?? _id,
+  title: title ?? _title,
+  body: body ?? _body,
+);
+  num? get userId => _userId;
+  num? get id => _id;
+  String? get title => _title;
+  String? get body => _body;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['userId'] = _userId;
+    map['id'] = _id;
+    map['title'] = _title;
+    map['body'] = _body;
+    return map;
+  }
+
+}
