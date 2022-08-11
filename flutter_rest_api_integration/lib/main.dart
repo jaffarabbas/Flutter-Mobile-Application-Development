4858b70b9a778c85f4534d37f3f0d83785ed7449
Jaffar Abbas <gamaportal8@gmail.com>
2022-08-11 21:03:38 +0500

    Commited File : main.dart at 8/11/2022 9:03:38 PM

--- flutter_rest_api_integration/lib/main.dart
+++ flutter_rest_api_integration/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_rest_api_integration/pages/home_page.dart';
+import 'package:flutter_rest_api_integration/pages/product_screen.dart';
+import 'package:flutter_rest_api_integration/pages/user_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,7 +12,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Api Integration',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
