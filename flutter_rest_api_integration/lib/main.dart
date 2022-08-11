edf15a101e17888f7e1961188e5066bdbe44f438
Jaffar Abbas <gamaportal8@gmail.com>
2022-08-11 13:40:59 +0500

    Commited File : main.dart at 8/11/2022 1:40:58 PM

---
+++ flutter_rest_api_integration/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_rest_api_integration/pages/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp();
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
