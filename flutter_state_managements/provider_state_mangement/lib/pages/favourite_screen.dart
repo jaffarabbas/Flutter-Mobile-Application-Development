29b3b63316d10db22f039fe723b85f53913c6756
Jaffar Abbas <gamaportal8@gmail.com>
2022-08-12 20:52:06 +0500

    Commited File : favourite_screen.dart at 8/12/2022 8:52:05 PM

--- flutter_state_managements/provider_state_mangement/lib/pages/favourite_screen.dart
+++ flutter_state_managements/provider_state_mangement/lib/pages/favourite_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_state_mangement/pages/favlist_screen.dart';
+import 'package:provider_state_mangement/provider/favourite_provider.dart';
 
 class Favouritepage extends StatefulWidget {
   Favouritepage({Key? key}) : super(key: key);
@@ -10,12 +13,46 @@ class Favouritepage extends StatefulWidget {
 class _FavouritepageState extends State<Favouritepage> {
   @override
   Widget build(BuildContext context) {
+    // final listProvider = Provider.of<FavouriteProvider>(context, listen: false);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Favourite Page'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.favorite),
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: ((context) => FavListScreen())));
+            },
+          ),
+        ],
       ),
-      body: Center(
-        child: Text('Favourite Page'),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: 100,
+              itemBuilder: (context, index) {
+                return Consumer<FavouriteProvider>(
+                  builder: (context, value, child) {
+                    return ListTile(
+                      onTap: () {
+                        if (value.seletItem.contains(index)) {
+                          value.removeSelectItem(index);
+                        } else {
+                          value.setSelectItem(index);
+                        }
+                      },
+                      title: Text('Item $index'),
+                      trailing: Icon(value.seletItem.contains(index)
+                          ? Icons.favorite
+                          : Icons.favorite_border),
+                    );
+                  },
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
