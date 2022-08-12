67652ceb6f3a3cdef302c78c8afaeb4ea7e5c70b
Jaffar Abbas <gamaportal8@gmail.com>
2022-08-12 16:08:11 +0500

    Commited File : main.dart at 8/12/2022 4:08:11 PM

---
+++ flutter_state_managements/provider_state_mangement/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_state_mangement/pages/count.dart';
+import 'package:provider_state_mangement/pages/favourite_screen.dart';
+import 'package:provider_state_mangement/provider/count_provider.dart';
+import 'package:provider_state_mangement/provider/slider_provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+         ChangeNotifierProvider(create:  (_) => CountProvider()),
+         ChangeNotifierProvider(create:  (_) => SliderProvider()),
+      ],
+      child: MaterialApp(
+        title: 'Provider Sate Mangement',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: Favouritepage(),
+      ),
+    );
+  }
+}
