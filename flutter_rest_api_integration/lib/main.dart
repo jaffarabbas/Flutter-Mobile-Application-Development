747a47b9cbd12cc43d5f878d578edd7cdfc967c4
Jaffar Abbas <gamaportal8@gmail.com>
2022-08-12 13:39:14 +0500

    Commited File : main.dart at 8/12/2022 1:39:13 PM

--- flutter_rest_api_integration/lib/main.dart
+++ flutter_rest_api_integration/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_rest_api_integration/pages/home_page.dart';
 import 'package:flutter_rest_api_integration/pages/product_screen.dart';
+import 'package:flutter_rest_api_integration/pages/signup_screen.dart';
+import 'package:flutter_rest_api_integration/pages/upload_image_screen.dart';
 import 'package:flutter_rest_api_integration/pages/user_page.dart';
 
 void main() {
@@ -18,7 +20,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const HomePage(),
+      home: UploadImage(),
     );
   }
 }
