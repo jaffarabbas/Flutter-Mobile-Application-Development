02dd5017b8da3a626097ffe5605cabc4c5fa7f5f
Jaffar Abbas <gamaportal8@gmail.com>
2022-08-13 02:16:17 +0500

    Commited File : theme_changer.dart at 8/13/2022 2:16:16 AM

---
+++ flutter_state_managements/provider_state_mangement/lib/provider/theme_changer.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class ThemeChangerProvider with ChangeNotifier {
+
+  var _themeMode = ThemeMode.light;
+
+  ThemeMode get themeMode => _themeMode; 
+
+  void setThemeMode(themeMode) {
+    _themeMode = themeMode;
+    notifyListeners();
+  }
+}
