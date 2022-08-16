9481c4e4024018d9e163ddff1c2f927e358c8676
Jaffar Abbas <gamaportal8@gmail.com>
2022-08-16 20:03:54 +0500

    Commited File : home_screen.dart at 8/16/2022 8:03:53 PM

---
+++ flutter_mvvm_architecture/lib/view/home_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+   return Scaffold(
+     appBar: AppBar(
+       title: const Text("Home"),
+     ),
+     body: Column(
+      children: [],
+     ),
+   );
+  }
+}
