a3ae33791bc76282cde012ec610e123dfc5991cd
Jaffar Abbas <gamaportal8@gmail.com>
2022-08-17 01:50:28 +0500

    Commited File : routes.dart at 8/17/2022 1:50:26 AM

--- flutter_mvvm_architecture/lib/utils/routes/routes.dart
+++ flutter_mvvm_architecture/lib/utils/routes/routes.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mvvm_architecture/utils/routes/routes_names.dart';
 import 'package:flutter_mvvm_architecture/view/login_view.dart';
+import 'package:flutter_mvvm_architecture/view/splash_view.dart';
 
 import '../../view/home_screen.dart';
 
@@ -15,6 +16,9 @@ class Routes {
       case RoutesName.login:
         return MaterialPageRoute(
             builder: (BuildContext context) => LoginView());
+      case RoutesName.splash:
+        return MaterialPageRoute(
+            builder: (BuildContext context) => SplashView());
       default:
         return MaterialPageRoute(
           builder: (_) {
