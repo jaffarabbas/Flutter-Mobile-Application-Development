af72781ef51cdefff2b341f8be0b3d3204d94cc8
Jaffar Abbas <gamaportal8@gmail.com>
2022-08-16 20:03:48 +0500

    Commited File : routes.dart at 8/16/2022 8:03:47 PM

---
+++ flutter_mvvm_architecture/lib/utils/routes/routes.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_mvvm_architecture/utils/routes/routes_names.dart';
+import 'package:flutter_mvvm_architecture/view/login_view.dart';
+
+import '../../view/home_screen.dart';
+
+class Routes {
+  static Route<dynamic> generateRoute(RouteSettings routeSettings) {
+    final arguments = routeSettings.arguments; 
+    switch (routeSettings.name) {
+      case RoutesName.home:
+        return MaterialPageRoute(
+            builder: (BuildContext context) => HomeScreen());
+      case RoutesName.login:
+        return MaterialPageRoute(
+            builder: (BuildContext context) => LoginView());
+      default:
+        return MaterialPageRoute(
+          builder: (_) {
+            return Scaffold(
+              body: Center(
+                child: Text("No route defined for ${routeSettings.name}"),
+              ),
+            );
+          },
+        );
+    }
+  }
+}
