7326a5f2e8e38d5af793899e856e775d50dd79d7
Jaffar Abbas <gamaportal8@gmail.com>
2022-08-16 23:53:45 +0500

    Commited File : colors.dart at 8/16/2022 11:53:41 PM

---
+++ flutter_mvvm_architecture/lib/res/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColor{
+  static const Color primaryColor = Color(0xFF3F51B5);
+  static const Color accentColor = Color(0xFF3F51B5);
+  static const Color primaryTextColor = Color(0xFF212121);
+  static const Color secondaryTextColor = Color(0xFFFFFFFF);
+  static const Color dividerColor = Color(0xFFBDBDBD);
+}
