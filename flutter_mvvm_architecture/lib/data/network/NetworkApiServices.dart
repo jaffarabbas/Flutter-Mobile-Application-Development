bd2f0812dabe786c6f9c95e3fa72fe78c25f2504
Jaffar Abbas <gamaportal8@gmail.com>
2022-08-16 20:03:39 +0500

    Commited File : NetworkApiServices.dart at 8/16/2022 8:03:38 PM

---
+++ flutter_mvvm_architecture/lib/data/network/NetworkApiServices.dart
@@ -0,0 +1,47 @@
+import 'dart:io';
+
+import 'package:flutter_mvvm_architecture/data/network/BaseApiServices.dart';
+
+import '../app_exceptions.dart';
+import 'package:http/http.dart' as http;
+
+class NetworkApiResponce extends BaseApiServices {
+  @override
+  Future getGetApiResponce(String url) async {
+    dynamic responseJson;
+    try{
+      final response = await http.get(Uri.parse(url)).timeout(Duration(seconds: 10));
+      responseJson = returnResponse(response);
+    }on SocketException {
+      throw FetchDataException('No Internet connection');
+    }
+    return responseJson;
+  }
+
+  @override
+  Future getPostApiResponce(String url,dynamic data) async {
+    dynamic responseJson;
+    try{
+      final response = await http.post(Uri.parse(url),body: data).timeout(Duration(seconds: 10));
+      responseJson = returnResponse(response);
+    }on SocketException {
+      throw FetchDataException('No Internet connection');
+    }
+    return responseJson;
+  }
+
+  dynamic returnResponse (http.Response response){
+    switch(response.statusCode){
+      case 200:
+        var responseJson = response.body;
+        return responseJson;
+      case 400:
+        throw BadRequestException(response.body.toString());
+      case 500:
+      case 404:
+        throw UnauthorisedException(response.body.toString());
+      default:
+        throw FetchDataException('Error occured while Communication with Server with StatusCode : ${response.statusCode}');
+    }
+  }
+}
