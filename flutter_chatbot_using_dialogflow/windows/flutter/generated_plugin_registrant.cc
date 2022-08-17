902606fca1d97e05d26dc1f7e3f23972c285004a
Jaffar Abbas <gamaportal8@gmail.com>
2022-08-17 21:22:51 +0500

    Commited File : generated_plugin_registrant.cc at 8/17/2022 9:22:50 PM

---
+++ flutter_chatbot_using_dialogflow/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <dialogflow_grpc/dialogflow_grpc_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  DialogflowGrpcPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("DialogflowGrpcPlugin"));
+}
