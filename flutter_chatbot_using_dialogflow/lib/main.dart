c4c2303d6988e09fcb932920e9a02d1e9ac13b16
Jaffar Abbas <gamaportal8@gmail.com>
2022-08-17 21:20:45 +0500

    Commited File : main.dart at 8/17/2022 9:20:44 PM

---
+++ flutter_chatbot_using_dialogflow/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_chatbot_using_dialogflow/chat.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Chat(),
+    ); 
+  }
+}
