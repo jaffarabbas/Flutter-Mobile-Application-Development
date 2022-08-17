bd2314ef7e9e433c2557092cf4a91fbf37c7c643
Jaffar Abbas <gamaportal8@gmail.com>
2022-08-17 21:21:07 +0500

    Commited File : GeneratedPluginRegistrant.swift at 8/17/2022 9:21:07 PM

---
+++ flutter_chatbot_using_dialogflow/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import dialogflow_grpc
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  DialogflowGrpcPlugin.register(with: registry.registrar(forPlugin: "DialogflowGrpcPlugin"))
+}
