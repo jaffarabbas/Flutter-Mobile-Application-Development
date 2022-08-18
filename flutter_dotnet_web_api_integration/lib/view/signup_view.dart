4f79c1448b497d80dbd82ddc37e1a98b909740cd
Jaffar Abbas <gamaportal8@gmail.com>
2022-08-18 22:35:28 +0500

    Commited File : signup_view.dart at 8/18/2022 10:35:27 PM

---
+++ flutter_dotnet_web_api_integration/lib/view/signup_view.dart
@@ -0,0 +1,174 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_dotnet_web_api_integration/res/components/defaultButton.dart';
+import 'package:flutter_dotnet_web_api_integration/utils/routes/routes_names.dart';
+import 'package:flutter_dotnet_web_api_integration/utils/utils.dart';
+import 'package:provider/provider.dart';
+import '../view model/auth_view_model.dart';
+
+class SigmUpView extends StatefulWidget {
+  const SigmUpView({Key? key}) : super(key: key);
+
+  @override
+  State<SigmUpView> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<SigmUpView> {
+  TextEditingController firstNameController = TextEditingController();
+  TextEditingController lastNameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController confirmPasswordController = TextEditingController();
+
+  FocusNode firstNameFocusNode = FocusNode();
+  FocusNode lastNameFocusNode = FocusNode();
+  FocusNode emailFocusNode = FocusNode();
+  FocusNode passwordFocusNode = FocusNode();
+  FocusNode confirmPasswordFocusNode = FocusNode();
+
+  ValueNotifier<bool> _obscureText = ValueNotifier<bool>(false);
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    firstNameController.dispose();
+    lastNameController.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+    confirmPasswordController.dispose();
+
+    firstNameFocusNode.dispose();
+    lastNameFocusNode.dispose();
+    emailFocusNode.dispose();
+    passwordFocusNode.dispose();
+    confirmPasswordFocusNode.dispose();
+    _obscureText.dispose();
+  }
+  @override
+  Widget build(BuildContext context) {
+    final authViewModel = Provider.of<AuthViewModel>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Login"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextFormField(
+              controller: firstNameController,
+              keyboardType: TextInputType.name,
+              focusNode: firstNameFocusNode,
+              decoration: const InputDecoration(
+                prefixIcon: Icon(Icons.person),
+                labelText: 'First Name',
+              ),
+              onFieldSubmitted: (feild) {
+                Utils.feildFocusChange(
+                    context, firstNameFocusNode, lastNameFocusNode);
+              },
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextFormField(
+              controller: emailController,
+              keyboardType: TextInputType.emailAddress,
+              focusNode: emailFocusNode,
+              decoration: const InputDecoration(
+                prefixIcon: Icon(Icons.email),
+                labelText: 'Email',
+              ),
+              onFieldSubmitted: (feild) {
+                Utils.feildFocusChange(
+                    context, emailFocusNode, passwordFocusNode);
+              },
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ValueListenableBuilder(
+              valueListenable: _obscureText,
+              builder: (context, falg, child) {
+                return TextFormField(
+                  controller: passwordController,
+                  obscureText: _obscureText.value,
+                  focusNode: passwordFocusNode,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.lock),
+                    labelText: 'Password',
+                    suffixIcon: InkWell(
+                      child: Icon(_obscureText.value
+                          ? Icons.visibility_off_outlined
+                          : Icons.visibility),
+                      onTap: () {
+                        _obscureText.value = !_obscureText.value;
+                      },
+                    ),
+                  ),
+                );
+              },
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ValueListenableBuilder(
+              valueListenable: _obscureText,
+              builder: (context, falg, child) {
+                return TextFormField(
+                  controller: confirmPasswordController,
+                  obscureText: _obscureText.value,
+                  focusNode: confirmPasswordFocusNode,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.lock),
+                    labelText: 'Confirm Password',
+                    suffixIcon: InkWell(
+                      child: Icon(_obscureText.value
+                          ? Icons.visibility_off_outlined
+                          : Icons.visibility),
+                      onTap: () {
+                        _obscureText.value = !_obscureText.value;
+                      },
+                    ),
+                  ),
+                );
+              },
+            ),
+            DefaultButton(title: 'Login', loading: authViewModel.authLoading, onPressed: (){
+              if(emailController.text.isEmpty && passwordController.text.isEmpty){
+                Utils.flushBarErrorMessage('Please Fill All Feilds', context);
+              }else if(passwordController.text.length < 6){
+                Utils.flushBarErrorMessage('Password Must Be 6 Characters', context);
+              }else{  
+                Map data = {
+                  // 'email': emailController.text,
+                  'email': 'eve.holt@reqres.in',
+                  // 'password': passwordController.text
+                  'password': 'cityslicka'
+                };
+                authViewModel.loginApi(data, context);
+              }
+            }),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                Text('Have An Account?'),
+                TextButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, RoutesName.login);
+                    },
+                    child: Text('Login'))
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
