2dc0b252c6618e20240f5bf467e27e49862823ac
Jaffar Abbas <gamaportal8@gmail.com>
2022-08-18 19:58:15 +0500

    Commited File : main.dart at 8/18/2022 7:58:14 PM

--- flutter_dotnet_web_api_integration/lib/main.dart
+++ flutter_dotnet_web_api_integration/lib/main.dart
@@ -1,21 +1,29 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_dotnet_web_api_integration/utils/routes/routes.dart';
+import 'package:flutter_dotnet_web_api_integration/utils/routes/routes_names.dart';
+import 'package:provider/provider.dart';
 void main() {
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => AuthViewModel()),
+        ChangeNotifierProvider(create: (context) => UserViewModel()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Mvvm',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        initialRoute: RoutesName.splash,
+        onGenerateRoute: Routes.generateRoute,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
