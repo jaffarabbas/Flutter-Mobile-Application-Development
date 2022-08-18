d34a82688976f10522fb93deef77f3740e0bb7ba
Jaffar Abbas <gamaportal8@gmail.com>
2022-08-18 19:57:55 +0500

    Commited File : utils.dart at 8/18/2022 7:57:54 PM

---
+++ flutter_dotnet_web_api_integration/lib/utils/utils.dart
@@ -0,0 +1,51 @@
+import 'package:another_flushbar/flushbar.dart';
+import 'package:another_flushbar/flushbar_route.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class Utils {
+
+  static void feildFocusChange(BuildContext context, FocusNode currentFocus, FocusNode nextFocus) {
+    currentFocus.unfocus();
+    FocusScope.of(context).requestFocus(nextFocus);
+  }
+
+
+  static toastMessage(String message) {
+    Fluttertoast.showToast(
+        msg: message,
+        backgroundColor: Colors.black,
+        textColor: Colors.white,
+        toastLength: Toast.LENGTH_LONG);
+  }
+
+  static void flushBarErrorMessage(String message, BuildContext context) {
+    showFlushbar(
+      context: context,
+      flushbar: Flushbar(
+        message: message,
+        backgroundColor: Colors.red,
+        title: 'Error',
+        messageColor: Colors.black87,
+        margin: const EdgeInsets.symmetric(vertical: 10),
+        duration: const Duration(seconds: 3),
+        reverseAnimationCurve: Curves.easeInOut,
+        positionOffset: 20,
+        icon: const Icon(
+          Icons.error,
+          color: Colors.white,
+        ),
+      )..show(context),
+    );
+  }
+
+  static snackBarGenerater(String message, BuildContext context) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        backgroundColor: Colors.red,
+        content: Text(message),
+        duration: const Duration(seconds: 3),
+      ),
+    );
+  }
+}
