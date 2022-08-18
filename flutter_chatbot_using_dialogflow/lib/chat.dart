8edd16194a77b9a804f234cecbeceec59268d1ce
Jaffar Abbas <gamaportal8@gmail.com>
2022-08-18 21:04:25 +0500

    Commited File : chat.dart at 8/18/2022 9:04:24 PM

--- flutter_chatbot_using_dialogflow/lib/chat.dart
+++ flutter_chatbot_using_dialogflow/lib/chat.dart
@@ -1,6 +1,7 @@
 import 'package:dialog_flowtter/dialog_flowtter.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_chatbot_using_dialogflow/messages.dart';
+
+import 'messages.dart';
 
 class Chat extends StatefulWidget {
   Chat({Key? key}) : super(key: key);
@@ -21,16 +22,40 @@ class _ChatState extends State<Chat> {
     super.initState();
   }
 
+  sendMessages(String text) async {
+    if(text.isEmpty){
+      print('Message is empty');
+    }else{
+      setState(() {
+        addMessage(Message(text: DialogText(text: [text])),true);
+      });
+
+      DetectIntentResponse response = await dialogFlowtter.detectIntent(queryInput: QueryInput(text: TextInput(text: text)));
+      if(response.message == null) return;
+      setState(() {
+        addMessage(response.message!);
+      });
+    }
+  }
+
+  addMessage(Message message, [bool isUserMessage = false]){
+    _messages.add({
+      'message': message,
+      'isUserMessage': isUserMessage
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.black87,
         title: Text('Chat'),
       ),
       body: Column(
         children: [
           Expanded(
-            child: Messages(messages: _messages),
+            child: MessagesScreen(messages: _messages),
           ),
           Container(
             padding: EdgeInsets.all(8),
@@ -46,7 +71,8 @@ class _ChatState extends State<Chat> {
                 ),
                 IconButton(
                   onPressed: () {
-                  
+                    sendMessages(_textController.text);
+                    _textController.clear();
                   },
                   icon: Icon(Icons.send),
                 ),
