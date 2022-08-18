940381087bd482989af1004d02f4cbec1fe143a1
Jaffar Abbas <gamaportal8@gmail.com>
2022-08-18 22:35:22 +0500

    Commited File : books_model.dart at 8/18/2022 10:35:21 PM

---
+++ flutter_dotnet_web_api_integration/lib/model/books_model.dart
@@ -0,0 +1,21 @@
+class BookModel {
+  int? id;
+  String? title;
+  String? description;
+
+  BookModel({this.id, this.title, this.description});
+
+  BookModel.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    title = json['title'];
+    description = json['description'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['title'] = this.title;
+    data['description'] = this.description;
+    return data;
+  }
+}
