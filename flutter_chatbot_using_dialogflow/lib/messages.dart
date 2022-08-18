154f6ed010218968658a77318a27e5af26d6b991
Jaffar Abbas <gamaportal8@gmail.com>
2022-08-18 21:04:28 +0500

    Commited File : messages.dart at 8/18/2022 9:04:27 PM

--- flutter_chatbot_using_dialogflow/lib/messages.dart
+++ flutter_chatbot_using_dialogflow/lib/messages.dart
@@ -1,16 +1,51 @@
 import 'package:flutter/material.dart';
 
-class Messages extends StatefulWidget {
+class MessagesScreen extends StatefulWidget {
   final List messages;
-  Messages({Key? key, required this.messages}) : super(key: key);
+  MessagesScreen({Key? key, required this.messages}) : super(key: key);
 
   @override
-  State<Messages> createState() => _MessagesState();
+  State<MessagesScreen> createState() => _MessagesScreenState();
 }
 
-class _MessagesState extends State<Messages> {
+class _MessagesScreenState extends State<MessagesScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    var w = MediaQuery.of(context).size.width;
+    return ListView.separated(
+        itemBuilder: (context, index) {
+          return Container(
+            margin: EdgeInsets.all(8),
+            child: Row(
+              mainAxisAlignment: widget.messages[index]['isUserMessage']
+                  ? MainAxisAlignment.end
+                  : MainAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 14, horizontal: 14),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(
+                          20,
+                        ),
+                        topRight: Radius.circular(20),
+                        bottomRight: Radius.circular(
+                            widget.messages[index]['isUserMessage'] ? 0 : 20),
+                        topLeft: Radius.circular(
+                            widget.messages[index]['isUserMessage'] ? 20 : 0),
+                      ),
+                      color: widget.messages[index]['isUserMessage']
+                          ? Colors.grey.shade800
+                          : Colors.grey.shade900.withOpacity(0.8)),
+                  constraints: BoxConstraints(maxWidth: w * 2 / 3),
+                  child: Text(widget.messages[index]['message'].text.text[0],style: TextStyle(color: Colors.white),),
+                ),
+              ],
+            ),
+          );
+        },
+        separatorBuilder: (_, i) => Padding(padding: EdgeInsets.only(top: 10)),
+        itemCount: widget.messages.length);
   }
 }
