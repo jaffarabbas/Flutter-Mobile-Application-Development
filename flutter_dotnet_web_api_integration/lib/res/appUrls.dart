8f4dbfa978c1cce4b041fd13388624a4b04dd38d
Jaffar Abbas <gamaportal8@gmail.com>
2022-08-18 22:35:37 +0500

    Commited File : appUrls.dart at 8/18/2022 10:35:36 PM

--- flutter_dotnet_web_api_integration/lib/res/appUrls.dart
+++ flutter_dotnet_web_api_integration/lib/res/appUrls.dart
@@ -1,5 +1,9 @@
 class AppUrl{
-  static const String baseUrl = "https://reqres.in/";
-  static const String login = baseUrl+"api/login";
-  static const String register = baseUrl+"api/register";
+  static const String baseUrl = "https://jzbookstore.azurewebsites.net/";
+  //user url
+  static const String login = baseUrl+"api/account/login";
+  static const String register = baseUrl+"api/account/register";
+
+  //book url
+  static const String bookUrl = baseUrl+"api/book/";
 }
