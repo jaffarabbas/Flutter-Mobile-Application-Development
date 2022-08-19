62ecc649a20d40d45e0f9fc05bb627e2fd1892ec
Jaffar Abbas <gamaportal8@gmail.com>
2022-08-19 19:09:47 +0500

    Commited File : NetworkApiServices.dart at 8/19/2022 7:09:47 PM

--- flutter_dotnet_web_api_integration/lib/data/network/NetworkApiServices.dart
+++ flutter_dotnet_web_api_integration/lib/data/network/NetworkApiServices.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:flutter_dotnet_web_api_integration/data/network/BaseApiServices.dart';
@@ -9,29 +10,33 @@ class NetworkApiService extends BaseApiServices {
   @override
   Future getGetApiResponce(String url) async {
     dynamic responseJson;
-    try{
-      final response = await http.get(Uri.parse(url)).timeout(Duration(seconds: 10));
+    try {
+      final response =
+          await http.get(Uri.parse(url)).timeout(Duration(seconds: 10));
       responseJson = returnResponse(response);
-    }on SocketException {
+    } on SocketException {
       throw FetchDataException('No Internet connection');
     }
     return responseJson;
   }
 
   @override
-  Future getPostApiResponce(String url,dynamic data) async {
+  Future getPostApiResponce(String url, dynamic data) async {
     dynamic responseJson;
-    try{
-      final response = await http.post(Uri.parse(url),body: data).timeout(Duration(seconds: 10));
+    try {
+      final response = await http.post(Uri.parse(url), body: jsonEncode(data), headers: {
+        "Accept": "application/json",
+        "content-type": "application/json"
+      }).timeout(Duration(seconds: 10));
       responseJson = returnResponse(response);
-    }on SocketException {
+    } on SocketException {
       throw FetchDataException('No Internet connection');
     }
     return responseJson;
   }
 
-  dynamic returnResponse (http.Response response){
-    switch(response.statusCode){
+  dynamic returnResponse(http.Response response) {
+    switch (response.statusCode) {
       case 200:
         var responseJson = response.body;
         return responseJson;
@@ -41,7 +46,8 @@ class NetworkApiService extends BaseApiServices {
       case 404:
         throw UnauthorisedException(response.body.toString());
       default:
-        throw FetchDataException('Error occured while Communication with Server with StatusCode : ${response.statusCode}');
+        throw FetchDataException(
+            'Error occured while Communication with Server with StatusCode : ${response.statusCode}');
     }
   }
 }
