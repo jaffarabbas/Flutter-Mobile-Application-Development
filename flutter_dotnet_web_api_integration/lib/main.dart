e7ce498fd89d8e1936b288d37cdd45b99a99bd01
Jaffar Abbas <gamaportal8@gmail.com>
2022-08-19 13:36:39 +0500

    Commited File : main.dart at 8/19/2022 1:36:38 PM

--- flutter_dotnet_web_api_integration/lib/main.dart
+++ flutter_dotnet_web_api_integration/lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        initialRoute: RoutesName.splash,
+        initialRoute: RoutesName.signup,
         onGenerateRoute: Routes.generateRoute,
       ),
     );
