b1b918f6708693d6ad225e19c351d8505c803b09
Jaffar Abbas <gamaportal8@gmail.com>
2022-08-19 19:10:00 +0500

    Commited File : home_screen.dart at 8/19/2022 7:09:58 PM

--- flutter_dotnet_web_api_integration/lib/view/home_screen.dart
+++ flutter_dotnet_web_api_integration/lib/view/home_screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_dotnet_web_api_integration/model/user_model.dart';
+import 'package:flutter_dotnet_web_api_integration/utils/routes/routes_names.dart';
+import 'package:flutter_dotnet_web_api_integration/view%20model/user_view_model.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -12,11 +16,16 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
+  final userPrefrence = Provider.of<UserViewModel>(context, listen: false);
    return Scaffold(
      appBar: AppBar(
        title: const Text("Home"),
        actions: [
-          IconButton(onPressed: (){}, icon: const Icon(Icons.logout))
+          IconButton(onPressed: () {
+            userPrefrence.removeUser().then((value) => {
+               Navigator.pushReplacementNamed(context, RoutesName.login)
+            });
+          }, icon: const Icon(Icons.logout))
        ],
      ),
      body: Column(
