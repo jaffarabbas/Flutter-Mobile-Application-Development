9bec6ae04306a753945dbef5b3ae2e7b4e2cecc2
Jaffar Abbas <gamaportal8@gmail.com>
2022-08-28 21:07:59 +0500

    Commited File : main.dart at 8/28/2022 9:07:58 PM

---
+++ stream_builder_practice/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Streams',
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
