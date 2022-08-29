81981fda41b88dd35b258197bd1741780fbc76a7
Jaffar Abbas <gamaportal8@gmail.com>
2022-08-29 11:32:36 +0500

    Commited file flutter_dotnet_web_api_integration/lib/view_model/auth_view_model.dart at 29-08-2022 11:32:35

---
+++ flutter_dotnet_web_api_integration/lib/view_model/auth_view_model.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_dotnet_web_api_integration/utils/routes/routes_names.dart';
+import 'package:flutter_dotnet_web_api_integration/utils/utils.dart';
+import 'package:flutter_dotnet_web_api_integration/view%20model/user_view_model.dart';
+import 'package:provider/provider.dart';
+
+import '../model/user_model.dart';
+import '../repository/auth_respository.dart';
+
+class AuthViewModel with ChangeNotifier {
+  final _authRepo = AuthRepository();
+
+  bool _authLoading = false;
+  bool get authLoading => _authLoading;
+
+  void setLoader(bool value) {
+    _authLoading = value;
+    notifyListeners();
+  }
+
+  Future<void> loginApi(dynamic data, BuildContext context) async {
+    setLoader(true);
+    _authRepo.loginApi(data).then((value) {
+      print("value: $value");
+      setLoader(false);
+      final userPrefrences = Provider.of<UserViewModel>(context, listen: false);
+      userPrefrences.saveUser(UserModel(token: value.toString()));
+      Utils.snackBarGenerater('Successfully Logged In', context);
+      Navigator.pushNamed(context, RoutesName.home);  
+      if (kDebugMode) {}
+    }).onError((error, stackTrace) {
+      setLoader(false);
+      if (kDebugMode) {
+        print(error);
+      }
+      Utils.flushBarErrorMessage(error.toString(), context);
+    });
+  }
+
+  Future<void> signUpApi(dynamic data, BuildContext context) async {
+    setLoader(true);
+    _authRepo.registerApi(data).then((value) {
+      setLoader(false);
+      if (kDebugMode) {
+        Utils.snackBarGenerater('Succsessfully Registered', context);
+        Navigator.pushNamed(context, RoutesName.home);
+      }
+    }).onError((error, stackTrace) {
+      if (kDebugMode) {
+        Utils.flushBarErrorMessage(error.toString(), context);
+      }
+    });
+  }
+}
