944b88040f5a96578d141a9ad19c8e5b86de35ec
Jaffar Abbas <gamaportal8@gmail.com>
2022-09-14 21:46:45 +0500

    Updated lib/view_model/services/splash_services.dart at 14/09/2022 21:46:45

--- flutter_dotnet_web_api_integration/lib/view_model/services/splash_services.dart
+++ flutter_dotnet_web_api_integration/lib/view_model/services/splash_services.dart
@@ -3,24 +3,23 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_dotnet_web_api_integration/model/user_model.dart';
 import 'package:flutter_dotnet_web_api_integration/utils/routes/routes_names.dart';
 import 'package:flutter_dotnet_web_api_integration/utils/utils.dart';
+import 'package:flutter_dotnet_web_api_integration/view_model/user_view_model.dart';
 
-import '../../view model/user_view_model.dart';
-
-class SplashServices{
+class SplashServices {
   Future<UserModel> getUserData() => UserViewModel().getUser();
-  
+
   void checkAuthentication(BuildContext context) async {
     getUserData().then((value) async {
       // await UserViewModel().removeUser();
-      if(value.token == 'null' || value.token == ''){
-        await Future.delayed(Duration(seconds:3));
+      if (value.token == 'null' || value.token == '') {
+        await Future.delayed(Duration(seconds: 3));
         Navigator.pushNamed(context, RoutesName.login);
-      }else{
-        await Future.delayed(Duration(seconds:3));
+      } else {
+        await Future.delayed(Duration(seconds: 3));
         Navigator.pushNamed(context, RoutesName.home);
       }
-    }).onError((error, stackTrace){
-      if(kDebugMode){
+    }).onError((error, stackTrace) {
+      if (kDebugMode) {
         print(error);
         Utils.flushBarErrorMessage(error.toString(), context);
       }
