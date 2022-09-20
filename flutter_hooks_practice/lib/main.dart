31132d831e56996760bab3066d198d1ac27d1ff1
Jaffar Abbas <54433038+jaffarabbas@users.noreply.github.com>
2022-09-20 18:20:34 +0500

    Inserted lib/main.dart at 20/09/2022 18:20:34

---
+++ flutter_hooks_practice/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks_practice/Screens/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
