08cb562839d9021b7633f1555b53ab0cd302ea25
Jaffar Abbas <54433038+jaffarabbas@users.noreply.github.com>
2022-10-01 16:11:05 +0500

    Commited file flutter_firebase_integration/lib/Screens/home-screens.dart at 01-10-2022 04:11:03

---
+++ flutter_firebase_integration/lib/Screens/home-screens.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomeScrren extends StatefulWidget {
+  const HomeScrren({super.key});
+
+  @override
+  State<HomeScrren> createState() => _HomeScrrenState();
+}
+
+class _HomeScrrenState extends State<HomeScrren> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('sd'),
+    );
+  }
+}
