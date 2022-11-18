8c68a5588d6681ba2b65f44e728e8d2d8e527fc9
jaffarabbas <gamaportal8@gmail.com>
2022-11-18 13:15:55 +0500

    Inserted lib/main.dart at 18/11/2022 13:15:54

---
+++ flutter_graph_implementation/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/widgets.dart';
+
+import 'Screens/home-screen.dart';
+
+void main(List<String> args) => runApp(MyApp()); 
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
