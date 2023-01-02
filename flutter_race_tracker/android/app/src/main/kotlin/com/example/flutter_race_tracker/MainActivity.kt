744e8086bb06b9482b786809a8a6c2c62113f444
Jaffar Abbas <gamaportal8@gmail.com>
2023-01-02 17:27:18 +0500

    1-2-2023

---
+++ flutter_race_tracker/android/app/src/main/kotlin/com/example/flutter_race_tracker/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_race_tracker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter_race_tracker/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_race_tracker/Screen/HomePage.dart';
+
+void main(List<String> args) {
+  runApp( const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage()
+    );
+  }
+}
